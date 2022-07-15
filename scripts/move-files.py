9bbdc7917d3b4792bd2e89ef1576a4ba87f4d3af
robertocannella <robertocannella@gmail.com>
2022-07-15 08:41:46 -0400

    implemented move-file.py

--- scripts/move-files.py
+++ scripts/move-files.py
@@ -3,9 +3,24 @@ import sys
 import subprocess
 
 
+#*********************************************************
+# PREREQUESITES:
+#
+# The 'find_files.sh' script in this directory is the argument passed to
+# this python script
+
+
+#*********************************************************
+# Open the file passed as an argument store into f variable
+#
 f = open(sys.argv[1], "r")
+
+
+#*********************************************************
+# Iterate throught the files, renamimg them.
+#
 for line in f.readlines():
- old_name = line.strip()
- new_name = old_name.replace("jane", "jdoe")
- subprocess.run(["mv", old_name, new_name])
-f.close()
+ old_name = line.strip()                        # strip leading or trailing white spaces
+ new_name = old_name.replace("jane", "jdoe")    # store new_name with replaced portion of string
+ subprocess.run(["mv", old_name, new_name])     # call system to run 'mv' to rename file
+f.close()                                       # close the file
