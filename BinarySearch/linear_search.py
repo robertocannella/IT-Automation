1370d18e417000633a65bc7f5877bde50bb7df2f
robertocannella <robertocannella@gmail.com>
2022-07-16 16:47:34 -0400

    Add main method

--- BinarySearch/linear_search.py
+++ BinarySearch/linear_search.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
-
 len = len(sys.argv)
-
-
 
 def linear_search(list, key):
     """If key is in the list returns its position in the list, otherwise returns -1."""
@@ -13,13 +10,15 @@ def linear_search(list, key):
             return i
     return -1
 
-if len > 1:
-    list = []
-    for thing in sys.argv:
-        list.append(thing)
-    key = list.pop()
-    print(list)
-    print("Key of {} is {}".format(key,linear_search(list, key)))
-else:
-    print("enter a list of numbers,  followed by a key")
-    sys.exit(1)
+def main():
+    if len > 1:
+        list = []
+        for thing in sys.argv:
+            list.append(thing)
+        key = list.pop()
+        print("Key of {} is {}".format(key,linear_search(list, key)))
+    else:
+        print("enter a list of numbers,  followed by a key")
+        sys.exit(1)
+
+main()
