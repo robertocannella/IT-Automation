09347ab8bd2cd46f23d304da482861f86f2b7ac9
robertocannella <robertocannella@gmail.com>
2022-07-21 14:41:19 -0400

    Add image name to json object

--- CapstoneProjects/IV/scripts/run.py
+++ CapstoneProjects/IV/scripts/run.py
@@ -7,7 +7,7 @@ import requests
 url = "http://[CHANGE_THIS]/fruits"
 
 HOME = os.path.expanduser("~")
-input_dir = HOME+"/supplier-data/images/"
+input_dir = HOME+"/supplier-data/descriptions/"
 files = os.listdir(input_dir)
 
 keys = ['name', 'weight', 'description', 'image_name']
@@ -18,10 +18,12 @@ def post_descriptions():
         with open(os.path.join(input_dir, file), 'r') as fh:
             for index,line in enumerate(fh):
                 items[keys[index]] = line.strip('\n')
-            items['weight'] = items['weight'].replace(' lbs', '')
+            items['weight'] = int(items['weight'].replace(' lbs', ''))
+            items['image_name'] = file.replace('txt','jpeg')
             response = requests.post(url, json=items)
             if not response.ok:
                 print("ERROR: ", response.status_code)
+            print('sent:', items)
 
 def main():
     post_descriptions()
