c35c2021bd7b43c90b8e4dd16d574cdc35abc56b
robertocannella <robertocannella@gmail.com>
2022-07-21 11:25:43 -0400

    Add supplier_image_upload.py

---
+++ CapstoneProjects/IV/scripts/supplier_image_upload.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import os
+import requests
+
+url = "[CHANGE THIS]/upload"
+
+HOME = os.path.expanduser("~")
+input_dir = HOME+"/supplier-data/images/"
+files = os.listdir(input_dir)
+
+
+def upload_images(files):
+    for file in files:
+        with open (file, 'rb') as opened:
+            r = requests.post(url, files ={'file': opened})
+            if not r.ok:
+                print('ERROR: ', r.status_code)
+
+def main():
+    upload_images(files)
+
+if __name__ == '__main__':
+    main()
